cf94c92d978457e5f3393f6ad522a82e03b80e52
Anton Trollbäck <anton@trollback.se>
2022-07-08 11:30:00 +0200

    Initial

---
+++ vite.config.js
@@ -0,0 +1,35 @@
+import { viteSingleFile } from 'vite-plugin-singlefile'
+import { minify } from 'html-minifier-terser'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [
+    minifyPlugin({
+      collapseWhitespace: true,
+      keepClosingSlash: true,
+      removeComments: true,
+      removeRedundantAttributes: true,
+      removeScriptTypeAttributes: true,
+      removeStyleLinkTypeAttributes: true,
+      useShortDoctype: true,
+      minifyCSS: true
+    }),
+    viteSingleFile({ useRecommendedBuildConfig: false })
+  ],
+})
+
+function minifyPlugin(opts) {
+  return {
+    name: 'vite:minify-html',
+    enforce: 'post',
+    async generateBundle(_, outBundle) {
+      await Promise.all(
+        Object.values(outBundle)
+          .filter((bundle) => /\.html$/.test(bundle.fileName))
+          .map(async function (bundle) {
+            bundle.source = await minify(bundle.source, opts)
+          })
+      )
+    }
+  }
+}
